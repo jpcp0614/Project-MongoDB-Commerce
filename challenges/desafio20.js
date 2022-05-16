5251348f8e48e180bb6b91f58e86c8f7847b95f5
jpcp0614 <jpcp0614@gmail.com>
2022-05-16 19:34:51 -0300

    corrige comentário

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,10 +1,10 @@
 // Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo
 
 /*
-Crie uma query que faça a remoção do primeiro
-ingrediente no sanduíche Quarteirão com Queijo.
+1) Crie uma query que faça a remoção do primeiro
+   ingrediente no sanduíche Quarteirão com Queijo.
 
-Crie uma query que retorne o nome e ingredientes de todos os documentos.
+2) Crie uma query que retorne o nome e ingredientes de todos os documentos.
 */
 
 db.produtos.updateOne(
